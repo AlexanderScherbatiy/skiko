94b709724273e519bcc24315294825a7e88d9ca9
AlexanderScherbatiy <alexander.scherbatiy@bell-sw.com>
2022-08-26 17:53:17 +0300

    Link with jawt lib

--- skiko/src/jvmMain/cpp/common/jawt.cc
+++ skiko/src/jvmMain/cpp/common/jawt.cc
@@ -31,73 +31,8 @@ bool check(JNIEnv* env, const char* msg) {
 }
 #endif
 
-#if SK_BUILD_FOR_WIN
-void findJdkHome(JNIEnv* env, wchar_t* path, int pathLength) {
-#else
-void findJdkHome(JNIEnv* env, char* path, int pathLength) {
-#endif
-    jclass systemClass = env->FindClass("java/lang/System");
-    jmethodID getPropertyMethod = env->GetStaticMethodID(
-        systemClass,
-        "getProperty",
-        "(Ljava/lang/String;)Ljava/lang/String;");
-    jstring javaHomeString = env->NewStringUTF("java.home");
-    jstring propertyString = (jstring) env->CallStaticObjectMethod(systemClass, getPropertyMethod, javaHomeString);
-#if SK_BUILD_FOR_WIN
-    const jchar* propertyChars = env->GetStringChars(propertyString, 0);
-    StringCchPrintfW(path, pathLength, L"%ls", propertyChars);
-    env->ReleaseStringChars(propertyString, propertyChars);
-#else
-    const char* propertyChars = env->GetStringUTFChars(propertyString, 0);
-    snprintf(path, pathLength, "%s", propertyChars);
-    env->ReleaseStringUTFChars(propertyString, propertyChars);
-#endif
-}
-
-extern "C" jboolean Skiko_GetAWT(JNIEnv *env, JAWT *awt) {
-  static JAWT_GetAWT_t func = nullptr;
-#if SK_BUILD_FOR_WIN
-  if (!func) {
-    wchar_t jdkHome[FILENAME_MAX];
-    findJdkHome(env, jdkHome, sizeof(jdkHome));
-    wchar_t path[FILENAME_MAX];
-    StringCchPrintfW(path, sizeof(path), L"%ls\\bin\\jawt.dll", jdkHome);
-    HMODULE lib = LoadLibraryW(path);
-    if (!lib) {
-      fprintf(stderr, "Cannot open %ls\n", path);
-      abort();
-    }
-    func = reinterpret_cast<JAWT_GetAWT_t>(GetProcAddress(lib, "JAWT_GetAWT"));
-    if (!func) {
-      fprintf(stderr, "Cannot find JAWT_GetAWT in %ls\n", path);
-      abort();
-    }
-  }
-#else
-    if (!func) {
-        char jdkHome[FILENAME_MAX];
-        findJdkHome(env, jdkHome, sizeof(jdkHome));
-        char path[FILENAME_MAX];
-        snprintf(path, sizeof(path), "%s/lib/libjawt%s", jdkHome,
-#if SK_BUILD_FOR_MAC
-             ".dylib"
-#else
-             ".so"
-#endif
-        );
-        void *lib = dlopen(path, RTLD_LAZY | RTLD_GLOBAL);
-        if (!lib) {
-            fprintf(stderr, "Cannot open %s: %s\n", path, dlerror());
-            abort();
-        }
-        func = reinterpret_cast<JAWT_GetAWT_t>(dlsym(lib, "JAWT_GetAWT"));
-        if (!func) {
-            fprintf(stderr, "Cannot find JAWT_GetAWT in %s\n", path);
-            abort();
-        }
-    }
-#endif
-    return func(env, awt);
+ extern "C" jboolean Skiko_GetAWT(JNIEnv *env, JAWT *awt) {
+    return JAWT_GetAWT(env, awt);
 }
 
 #endif
